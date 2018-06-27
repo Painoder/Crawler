8d627c5c71ffe7ba4644adcf99b035767ef9c7e6
Painoder <836232013@qq.com>
2018-06-27 21:02:43 +0800

    代码结构更新

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Crawler
 Crawler for the school teacher information
+
+#DOM操作详细资料
+https://www.cnblogs.com/zhangyinhua/p/8037599.html
--- src/crawler/Jsoup_for_teacher.java
+++ src/crawler/Jsoup_for_teacher.java
@@ -1,7 +1,28 @@
 package crawler;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class Jsoup_for_teacher {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		
+		Document doc = null;
+		String url = "";
+		
+		//获取目标url
+		try {
+			/*
+			 * 模拟post请求
+			 * doc= Jsoup.connect(url).data("", "").userAgent("").cookie("auth", "token").timeout(1000).post(); 
+			 */
+			//doc = Jsoup.parse(file, "GBK"); 从本地文件获取页面
+			doc = Jsoup.connect(url).timeout(1000).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//DOM处理
 	}
 }
